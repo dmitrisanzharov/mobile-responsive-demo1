3c89fa7a1f8c8a4c1991c360f36800e4551e157f
dmitrisanzharov <68742189+dmitrisanzharov@users.noreply.github.com>
2021-08-29 09:06:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onload = imageStacker;
+
+
+function imageStacker(){
+    document.querySelector('#img1').src = 'https://github.com/kevin-powell/responsive-made-easy/blob/master/img/image-01.jpg?raw=true';
+
+    document.querySelector('#img2').src = 'https://github.com/kevin-powell/responsive-made-easy/blob/master/img/image-02.jpg?raw=true'
+
+
+    // end of imageStacker()
+}
